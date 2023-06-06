2de686088db0631936fe15662313bf39af6b8b1c
k-mitani <k-mitani@unique21.co.jp>
2023-06-06 23:48:11 +0900

    ノート一覧 並べ替え機能

--- app/home/NoteListView.tsx
+++ app/home/NoteListView.tsx
@@ -1,15 +1,19 @@
 import {format} from "date-fns";
-import React from "react";
+import React, {useState} from "react";
 
-function dateToText(date: string) {
-  if (date == null) return "";
+function parseDate(date: string): Date {
   const year = parseInt(date.substring(0, 4));
   const month = parseInt(date.substring(4, 6)) - 1; // 月は0から11で表現されるため、1を引きます
   const day = parseInt(date.substring(6, 8));
   const hour = parseInt(date.substring(9, 11));
   const minute = parseInt(date.substring(11, 13));
   const second = parseInt(date.substring(13, 15));
-  const d = new Date(Date.UTC(year, month, day, hour, minute, second));
+  return new Date(Date.UTC(year, month, day, hour, minute, second))
+}
+
+function dateToText(date: string) {
+  if (date == null) return "";
+  const d = parseDate(date);
   return format(d, 'yyyy/MM/dd');
 }
 
@@ -27,20 +31,58 @@ function NoteCard({note}: any) {
     </div>);
 }
 
+
+const orderItems = [
+  ["更新順↓", (a: any, b: any) => parseDate(b.UpdatedAt ?? b.CreatedAt).getTime() - parseDate(a.UpdatedAt ?? a.CreatedAt).getTime()],
+  ["更新順↑", (a: any, b: any) => parseDate(a.UpdatedAt ?? a.CreatedAt).getTime() - parseDate(b.UpdatedAt ?? b.CreatedAt).getTime()],
+  ["作成順↓", (a: any, b: any) => parseDate(b.CreatedAt).getTime() - parseDate(a.CreatedAt).getTime()],
+  ["作成順↑", (a: any, b: any) => parseDate(a.CreatedAt).getTime() - parseDate(b.CreatedAt).getTime()],
+  ["名前順↓", (a: any, b: any) => a.Title.localeCompare(b.Title)],
+  ["名前順↑", (a: any, b: any) => b.Title.localeCompare(a.Title)],
+];
+
 export default function NoteListView({notes, selectedNote, setSelectedNote}: any) {
   const noteCount = notes?.length ?? 0;
+  const [selectedOrder, setSelectedOrder] = useState(0);
+  const orderName = (String)(orderItems[selectedOrder][0]);
+  const orderFunc = orderItems[selectedOrder][1];
+  notes = notes?.sort(orderFunc);
+
+  const [showOrderItems, setShowOrderItems] = useState(false);
+
+  function onSelectOrder(i: number) {
+    setSelectedOrder(i);
+    // setShowOrderItems(false);
+  }
 
   return (
     <div className='flex-none w-72 h-screen overflow-y-scroll bg-gray-100'>
       <div className={"p-2 border-b-2 border-gray-400"}>
         <h2>ノート一覧 ({noteCount})</h2>
-        <button className={"text-sm m-1 p-0.5 bg-gray-500 text-white"}>更新順↑</button>
+        <span className={""}>
+          <span className={[
+            "bg-white border-2 border-gray-600",
+            "block absolute ml-[-80px]",
+            showOrderItems ? "" : " hidden",
+          ].join(" ")}>
+            <ul>
+              {orderItems.map((order: any, i) =>
+                <li key={i}
+                    className={"hover:bg-blue-300 p-0.5 cursor-pointer"}
+                    onClick={() => setSelectedOrder(i)}>{order[0]}</li>
+              )}
+            </ul>
+          </span>
+        </span>
+        <button className={"text-sm m-1 p-0.5 bg-gray-500 text-white"}
+                onClick={() => setShowOrderItems(!showOrderItems)}>
+          {orderName}
+        </button>
         <button className={"text-sm m-1 p-0.5 bg-gray-500 text-white"}>サマリー</button>
-        <input className={"m-1 border-2"} type="text" placeholder="ノートを検索" />
+        <input className={"m-1 border-2"} type="text" placeholder="ノートを検索"/>
       </div>
       <ul>
         {notes?.map((note: any, i: number) => {
-          console.log(note)
           return (
             <li key={note.name + "-" + i}
                 onMouseDown={() => setSelectedNote(note)}>
