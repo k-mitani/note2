e1476eeb02c593e8cfe874146c4ed9b6885a229a
k-mitani <k-mitani@unique21.co.jp>
2023-06-18 15:26:43 +0900

    フォルダー改造

--- app/home/SideBar.tsx
+++ app/home/SideBar.tsx
@@ -6,32 +6,66 @@ import classNames from "classnames";
 
 type FolderAndChild = Folder & { childFolders: FolderAndChild[] };
 
-function Folder({folder, selectedFolder, setSelectedFolder}: {
+function Header() {
+  return <>
+    <ul className="flex-col">
+      <li>
+        <button className="hover:bg-gray-600 w-full text-start">➕新規ノートブック</button>
+      </li>
+      <li>
+        <button className="hover:bg-gray-600 w-full text-start">🔖ショートカット</button>
+      </li>
+    </ul>
+  </>
+}
+
+
+const INDENT_WIDTH = 5;
+
+function Folder({folder, selectedFolder, setSelectedFolder, indent}: {
   folder: FolderAndChild,
   selectedFolder: FolderAndChild | undefined,
   setSelectedFolder: (folder: FolderAndChild) => void,
+  indent: number,
 }) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const hasChildren = folder.childFolders?.length > 0 ?? false;
   return (
     <>
-      <strong className={"block hover:bg-gray-500 cursor-pointer"}
-              onClick={() => setIsExpanded(!isExpanded)}>
-        {isExpanded ? "▶" : "▼"}
-        📘{folder.name}
-      </strong>
-      <ul className={classNames({hidden: isExpanded})}>
+      {/*フォルダー項目*/}
+      <div className={classNames(
+        "cursor-pointer flex select-none",
+        "ps-" + indent,
+        selectedFolder?.id === folder.id ? "bg-gray-500" : "hover:bg-gray-600",
+      )}>
+        {/*サブフォルダー展開ボタン*/}
+        <span className={classNames(
+          "flex-col hover:bg-gray-500 w-5",
+          {"hidden": !hasChildren}
+        )} onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? "▶" : "▼"}
+        </span>
+
+        {/*フォルダー名*/}
+        <span className={classNames(
+          "flex-col w-full",
+        )} onClick={() => setSelectedFolder(folder)}>
+          {folder.name}
+        </span>
+      </div>
+
+      {/*サブフォルダー*/}
+      {folder.childFolders && <ul className={classNames({hidden: isExpanded})}>
         {folder.childFolders.map(subFolder => {
-          return <li
-            key={subFolder.id}
-            onClick={() => setSelectedFolder(subFolder)}
-            className={classNames(
-              "ps-5 cursor-pointer",
-              selectedFolder?.name === subFolder.name ? "bg-gray-500" : "hover:bg-gray-600",
-            )}>
-            {subFolder.name}
+          return <li key={subFolder.id}>
+            <Folder folder={subFolder}
+                    selectedFolder={selectedFolder}
+                    setSelectedFolder={setSelectedFolder}
+                    indent={indent + INDENT_WIDTH}
+            />
           </li>
         })}
-      </ul>
+      </ul>}
     </>
   );
 }
@@ -46,24 +80,22 @@ export default function SideBar({folders}: {
   const [selectedFolder, setSelectedFolder] = useRecoilState(atoms.selectedFolder);
 
   return (
-    <div className='p-2 flex-none flex flex-col w-72 bg-gray-700 text-white h-screen'>
-      <ul className="flex-col">
-        <li>
-          <button className="hover:bg-gray-500 w-full text-start">➕新規ノートブック</button>
-        </li>
-        <li>
-          <button className="hover:bg-gray-500 w-full text-start">🔖ショートカット</button>
-        </li>
-      </ul>
+    <div className='p-0.5 flex-none flex flex-col w-72 bg-gray-700 text-white h-screen'>
+      {/*固定ヘッダー*/}
+      <Header></Header>
+
+      {/*フォルダー一覧*/}
       <ul className='mt-4 flex-col overflow-y-scroll'>
         {folders.map(folder => {
           return <li key={folder.id}>
             <Folder folder={folder as any}
                     selectedFolder={selectedFolder as any}
-                    setSelectedFolder={setSelectedFolder as any}/>
+                    setSelectedFolder={setSelectedFolder as any}
+                    indent={0}
+            />
           </li>
         })}
       </ ul>
     </div>
-  )
+  );
 }
