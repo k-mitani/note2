63a0df72614f009803c1c897d59a2f5b5c015965
k-mitani <k-mitani@unique21.co.jp>
2023-07-13 23:16:28 +0900

    ノート一覧 整理

--- app/home/NoteListView.tsx
+++ app/home/NoteListView.tsx
@@ -1,22 +1,32 @@
-import {format} from "date-fns";
 import React, {useEffect, useMemo, useRef, useState} from "react";
 import * as utils from "@/app/utils";
-import {z} from "zod";
 import {Note} from "@prisma/client";
 import {useRecoilState, useRecoilValue} from "recoil";
 import {atoms} from "@/app/home/atoms";
 import classNames from "classnames";
 import {useRecoilLocalStorage} from "@/app/utils";
+import {tr} from "date-fns/locale";
 
-function NoteCard({note, changed, isSelected}: {
+function NoteCard({note, setShouldScroll, setSelectedNote, _ref, changed, isSelected, onKeyDown}: {
   note: Note,
+  setShouldScroll: (b: boolean) => void,
+  setSelectedNote: (note: Note) => void,
+  _ref: React.Ref<HTMLButtonElement>,
   changed: { title: string, content: string } | undefined,
-  isSelected: boolean
+  isSelected: boolean,
+  onKeyDown: (e: React.KeyboardEvent<HTMLButtonElement>) => void,
 }) {
   const dateText = utils.dateToText(note.updatedAt ?? note.createdAt);
   const text = (changed ?? note).content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "")
   return (
-    <div className={"border-gray-300 border-b-2"}>
+    <button className="w-full text-start block border-gray-300 border-b-2"
+            onClick={() => {
+              setShouldScroll(false);
+              setSelectedNote(note)
+            }}
+            onFocus={() => setSelectedNote(note)}
+            onKeyDown={onKeyDown}
+            ref={_ref}>
       <div className={
         "flex flex-col hover:bg-white hover:border-cyan-400 border-2 p-2"
         + (isSelected ? " border-blue-500 bg-white" : " border-gray-100")
@@ -25,7 +35,7 @@ function NoteCard({note, changed, isSelected}: {
         <div className={"mt-2 h-16 line-clamp-3 text-gray-600 text-sm"}>{text}</div>
         <div className={"mt-2 text-[12px] text-gray-500"}>{dateText}</div>
       </div>
-    </div>);
+    </button>);
 }
 
 
@@ -38,13 +48,17 @@ const orderItems = [
   ["名前順↑", (a: Note, b: Note) => b.title.localeCompare(a.title)],
 ];
 
+/**
+ * ノート一覧
+ * @param notes
+ */
 export default function NoteListView({notes}: {
   notes: Note[] | null,
 }) {
   const [selectedNote, setSelectedNote] = useRecoilState(atoms.selectedNote);
   const [selectedOrder, setSelectedOrder] = useState(0);
+  const [shouldScroll, setShouldScroll] = useState(true);
   const [showOrderItems, setShowOrderItems] = useState(false);
-  const [shouldScroll, setShouldScroll] = useState(false);
   const [[changedNotes], setChangedNotes] = useRecoilState(atoms.changedNotes);
   const [showNoteListView, setShowNoteListView] = useRecoilLocalStorage(atoms.showNoteListView);
 
@@ -58,45 +72,51 @@ export default function NoteListView({notes}: {
     return notes.sort(orderFunc as any);
   }, [notes, selectedOrder]);
 
+  useEffect(() => {
+    console.log("scroll b", refSelectedNoteElement.current)
+    if (refSelectedNoteElement.current == null) return;
+    (refSelectedNoteElement.current as HTMLElement).focus();
+    console.log("scroll", refSelectedNoteElement.current)
+    if (shouldScroll) {
+      refSelectedNoteElement.current.scrollIntoView({block: "center"});
+      setShouldScroll(false);
+    }
+  }, [refSelectedNoteElement.current]);
+
   if (notes == null) return <div>loading...</div>
 
   console.log("render notes");
 
-  function onKeyDoen(ev: React.KeyboardEvent) {
+  function onKeyDown(ev: React.KeyboardEvent) {
     if (notes == null) return;
-    if (selectedNote == null) {
-      setSelectedNote(notes[0]);
-      setShouldScroll(true);
-    } else if (ev.key === "ArrowDown") {
-      const nextNote = notes[Math.min(notes.indexOf(selectedNote) + 1, notes.length - 1)];
-      setSelectedNote(nextNote);
-      setShouldScroll(true);
-    } else if (ev.key === "ArrowUp") {
-      const nextNote = notes[Math.max(notes.indexOf(selectedNote) - 1, 0)];
-      setSelectedNote(nextNote);
-      setShouldScroll(true);
-    }
-  }
 
-  if (shouldScroll && refSelectedNoteElement.current != null) {
-    refSelectedNoteElement.current.scrollIntoView({block: "center"});
-    setShouldScroll(false);
+    let index = 0;
+    if (selectedNote == null) {
+      index = 0;
+    } else if (ev.key === "ArrowDown") {
+      index = Math.min(notes.indexOf(selectedNote) + 1, notes.length - 1);
+    } else if (ev.key === "ArrowUp") {
+      index = Math.max(notes.indexOf(selectedNote) - 1, 0);
+    }
+    const nextNote = notes[index];
+    // setSelectedNote(nextNote);
+    (document.querySelector(`#note-${index} button`) as HTMLElement).focus();
   }
 
   return (
-    <div className={classNames('flex-none w-72 overflow-y-scroll bg-gray-100',
+    <div className={classNames('flex flex-col flex-none w-72 bg-gray-100',
       {'hidden': !showNoteListView},
-    )}
-         tabIndex={0}
-         onKeyDown={onKeyDoen}>
-      <div className={"p-2 border-b-2 border-gray-400"}>
+    )}>
+      {/*ヘッダー*/}
+      <div className={"flex-none p-1 border-b-2 border-gray-400"}>
         <h2>ノート一覧 ({noteCount})</h2>
-        <span className={""}>
-          <span className={[
+        {/*ソート設定リスト*/}
+        <span>
+          <span className={classNames(
             "bg-white border-2 border-gray-600",
             "block absolute ml-[-80px]",
             showOrderItems ? "" : " hidden",
-          ].join(" ")}>
+          )}>
             <ul>
               {orderItems.map((order: any, i) =>
                 <li key={i}
@@ -106,21 +126,31 @@ export default function NoteListView({notes}: {
             </ul>
           </span>
         </span>
+        {/*ソートボタン*/}
         <button className={"text-sm m-1 p-0.5 bg-gray-500 text-white"}
                 onClick={() => setShowOrderItems(!showOrderItems)}>
           {orderName}
         </button>
+
+        {/*表示形式*/}
         <button className={"text-sm m-1 p-0.5 bg-gray-500 text-white"}>サマリー</button>
+
+        {/*検索*/}
         <input className={"m-1 border-2"} type="text" placeholder="ノートを検索"/>
       </div>
-      <ul>
+
+      {/*一覧*/}
+      <ul className="flex-grow overflow-y-scroll">
         {notes?.map((note: any, i: number) => {
           return (
-            <li key={note.name + "-" + i}
-                onMouseDown={() => setSelectedNote(note)}
-                ref={selectedNote === note ? refSelectedNoteElement : null as any}
-            >
-              <NoteCard note={note} changed={changedNotes.get(note.id)} isSelected={selectedNote === note}></NoteCard>
+            <li key={note.name + "-" + i} id={`note-${i}`}>
+              <NoteCard note={note}
+                        setShouldScroll={setShouldScroll}
+                        setSelectedNote={setSelectedNote}
+                        _ref={selectedNote === note ? refSelectedNoteElement : null as any}
+                        onKeyDown={onKeyDown}
+                        changed={changedNotes.get(note.id)}
+                        isSelected={selectedNote === note}/>
             </li>);
         })}
       </ ul>
