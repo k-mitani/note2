673009c6d9edc8ab963e067661d2d24e0d242950
k-mitani <k-mitani@unique21.co.jp>
2023-07-29 14:57:29 +0900

    PWA化

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <link rel="icon" href="/icon.png" />
+        <meta name="theme-color" content="#444" />
+        <title>note2</title>
+      </head>
       <body className='h-screen'>{children}</body>
     </html>
   )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: false,
+  disable: process.env.NODE_ENV === 'development',
+})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = withPWA({
+  reactStrictMode: true,
+});
 
 module.exports = nextConfig
