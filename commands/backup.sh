5fcc7ef622a63d169f748b6ba575ab103d655223
k-mitani <k-mitani@unique21.co.jp>
2024-09-07 21:57:39 +0900

    コマンド追加

---
+++ commands/backup.sh
@@ -0,0 +1,1 @@
+sudo -u postgres pg_dump -d note2db -F custom > backup.custom
---
+++ commands/trim-content.sql
@@ -0,0 +1,43 @@
+
+UPDATE "Note"
+SET content =
+        replace(
+                replace(
+                        replace(
+                                content,
+                                '--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-gradient-from-position: ; --tw-gradient-via-position: ; --tw-gradient-to-position: ; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgba(59,130,246,.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ;',
+                                ''),
+                        'background-color: rgb(255 255 255/var(--tw-bg-opacity));',
+                        ''),
+                ' style=""',
+                ''
+        )
+WHERE content LIKE '%tw-%';
+
+-- SELECT LENGTH(content) / 1024 AS size_kb
+--      , "Folder".name          AS folder_name
+--      , title
+--      , content
+--      , replace(
+--         replace(
+--                 replace(
+--                         content,
+--                         '--tw-border-spacing-x: 0; --tw-border-spacing-y: 0; --tw-translate-x: 0; --tw-translate-y: 0; --tw-rotate: 0; --tw-skew-x: 0; --tw-skew-y: 0; --tw-scale-x: 1; --tw-scale-y: 1; --tw-pan-x: ; --tw-pan-y: ; --tw-pinch-zoom: ; --tw-scroll-snap-strictness: proximity; --tw-gradient-from-position: ; --tw-gradient-via-position: ; --tw-gradient-to-position: ; --tw-ordinal: ; --tw-slashed-zero: ; --tw-numeric-figure: ; --tw-numeric-spacing: ; --tw-numeric-fraction: ; --tw-ring-inset: ; --tw-ring-offset-width: 0px; --tw-ring-offset-color: #fff; --tw-ring-color: rgba(59,130,246,.5); --tw-ring-offset-shadow: 0 0 #0000; --tw-ring-shadow: 0 0 #0000; --tw-shadow: 0 0 #0000; --tw-shadow-colored: 0 0 #0000; --tw-blur: ; --tw-brightness: ; --tw-contrast: ; --tw-grayscale: ; --tw-hue-rotate: ; --tw-invert: ; --tw-saturate: ; --tw-sepia: ; --tw-drop-shadow: ; --tw-backdrop-blur: ; --tw-backdrop-brightness: ; --tw-backdrop-contrast: ; --tw-backdrop-grayscale: ; --tw-backdrop-hue-rotate: ; --tw-backdrop-invert: ; --tw-backdrop-opacity: ; --tw-backdrop-saturate: ; --tw-backdrop-sepia: ;',
+--                         ''),
+--                 'background-color: rgb(255 255 255/var(--tw-bg-opacity));',
+--                 ''),
+--         ' style=""',
+--         ''
+--        )                      AS new_content
+-- FROM "Note"
+--          JOIN "Folder" ON "folderId" = "Folder".id
+-- WHERE content LIKE '%tw-%'
+
+
+
+
+-- base64な画像の検索
+-- SELECT name, LENGTH(content) / 1024 as kb, *
+-- FROM "Note"
+-- JOIN "Folder" ON "Note"."folderId" = "Folder".id
+-- WHERE content LIKE '%<img src="data:image/%'
