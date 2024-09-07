e308dac8973871a470e840a074100efeaee6d1fc
k-mitani <k-mitani@unique21.co.jp>
2024-09-08 00:53:34 +0900

    関数をhook化

--- app/home/hooks.ts
+++ app/home/hooks.ts
@@ -1,5 +1,7 @@
-import useSWR from "swr";
+import useSWR, {mutate} from "swr";
 import {Folder, Note} from "@prisma/client";
+import {useNote} from "@/app/home/state";
+import * as utils from "@/app/utils";
 
 type FolderAndChild = Folder & { childFolders: FolderAndChild[] };
 
@@ -23,4 +25,86 @@ export function useFolderAndNotes(folderId: number | undefined) {
     });
   }
   return swr;
+}
+
+export function useSaveChanges(mutateNotesParent: () => Promise<void>) {
+  const [changedNotes] = useNote(state => state.changedNotes);
+  const clearChangedNotes = useNote(state => state.clearChangedNotes);
+
+  return async function saveChanges() {
+    console.log("saveChanges", changedNotes);
+    await fetch("/api/rpc/saveChanges", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        notes: Array.from(changedNotes.values()),
+      }),
+    });
+    clearChangedNotes()
+    await Promise.all([
+      mutateNotesParent(),
+      mutate("/api/rpc/getFoldersAll"),
+    ]);
+  }
+}
+
+export function useOnCreateNewNote(mutateNotesParent: () => Promise<void>) {
+  const selectedFolder = useNote(state => state.selectedFolder);
+  const setSelectedNote = useNote(state => state.setSelectedNote);
+
+  return async function onCreateNewNote() {
+    if (selectedFolder == null) return;
+    const res = await fetch(
+      `/api/folders/${selectedFolder.id}/createNote`,
+      {method: "POST"}
+    );
+    const newNote = await res.json();
+    utils.coerceDate(newNote, "createdAt");
+    utils.coerceDate(newNote, "updatedAt");
+    console.log(newNote);
+    await Promise.all([
+      mutateNotesParent(),
+      mutate("/api/rpc/getFoldersAll"),
+    ]);
+    setSelectedNote(newNote);
+  }
+}
+
+export function useOnDropToFolder(mutateNotesParent: () => Promise<void>) {
+  return async function onDropToFolder(
+    ev: { target: Folder, notes: Note[] | null, folders: Folder[] | null }
+  ) {
+    // ノートがドロップされた場合
+    if (ev.notes != null) {
+      await fetch("/api/rpc/moveNotes/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          folderId: ev.target.id,
+          noteIds: ev.notes.map(n => n.id),
+        })
+      });
+
+      mutateNotesParent();
+      mutate("/api/rpc/getFoldersAll");
+    }
+    // フォルダーがドロップされた場合
+    if (ev.folders != null) {
+      await fetch("/api/rpc/moveFolders/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          parentFolderId: ev.target.id,
+          folderIds: ev.folders.map(n => n.id).filter(id => id !== ev.target.id),
+        })
+      });
+
+      mutateNotesParent();
+      mutate("/api/rpc/getFoldersAll");
+    }
+  }
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import React, {useCallback, useEffect} from "react";
-import useSWR, {mutate} from "swr";
+import React, {useEffect} from "react";
 import FolderListView from "@/app/home/components/FolderListView";
-import * as utils from "@/app/utils";
 import NoteEditor from "@/app/home/components/NoteEditor";
 import NoteListView from "@/app/home/components/NoteListView";
 import {useNote} from "@/app/home/state";
-import {Folder, Note} from "@prisma/client";
-import {useFolderAndNotes, useFoldersAll} from "@/app/home/hooks";
+import {useFolderAndNotes, useFoldersAll, useOnCreateNewNote, useOnDropToFolder, useSaveChanges} from "@/app/home/hooks";
 import {Header} from "@/app/home/components/Header";
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
@@ -18,14 +15,14 @@ import {HotkeysProvider} from "react-hotkeys-hook";
 function HomeInternal() {
   // State
   const selectedFolder = useNote(state => state.selectedFolder);
-  const [changedNotes] = useNote(state => state.changedNotes);
   const setSelectedFolder = useNote(state => state.setSelectedFolder);
-  const setSelectedNote = useNote(state => state.setSelectedNote);
-  const clearChangedNotes = useNote(state => state.clearChangedNotes);
 
   // Data
   const {data: folders, error, isLoading} = useFoldersAll();
   const {data: notesParent, mutate: mutateNotesParent} = useFolderAndNotes(selectedFolder?.id);
+  const saveChanges = useSaveChanges(mutateNotesParent as any);
+  const onCreateNewNote = useOnCreateNewNote(mutateNotesParent as any);
+  const onDropToFolder = useOnDropToFolder(mutateNotesParent as any);
 
   // 初回のみ、最初のフォルダを選択する。
   useEffect(() => {
@@ -34,78 +31,10 @@ function HomeInternal() {
     }
   }, [folders, selectedFolder, setSelectedFolder]);
 
-
   // 読み込み中なら何もしない。
   if (error) return <div className="h-full w-screen bg-gray-700 text-white">failed to load</div>
   if (isLoading) return <div className="h-full w-screen bg-gray-700 text-white">loading...</div>
   if (folders == null) return <div className="h-full w-screen bg-gray-700 text-white">folders is null</div>
-
-  async function onCreateNewNote() {
-    if (selectedFolder == null) return;
-    const res = await fetch(
-      `/api/folders/${selectedFolder.id}/createNote`,
-      {method: "POST"}
-    );
-    const newNote = await res.json();
-    utils.coerceDate(newNote, "createdAt");
-    utils.coerceDate(newNote, "updatedAt");
-    console.log(newNote);
-    await Promise.all([
-      mutateNotesParent(),
-      mutate("/api/rpc/getFoldersAll"),
-    ]);
-    setSelectedNote(newNote);
-  }
-
-  async function saveChanges() {
-    console.log("saveChanges", changedNotes);
-    await fetch("/api/rpc/saveChanges", {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({
-        notes: Array.from(changedNotes.values()),
-      }),
-    });
-    clearChangedNotes()
-    await Promise.all([
-      mutateNotesParent(),
-      mutate("/api/rpc/getFoldersAll"),
-    ]);
-  }
-
-  async function onDropToFolder(ev: { target: Folder, notes: Note[] | null, folders: Folder[] | null }) {
-    if (ev.notes != null) {
-      await fetch("/api/rpc/moveNotes/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          folderId: ev.target.id,
-          noteIds: ev.notes.map(n => n.id),
-        })
-      });
-
-      mutateNotesParent();
-      mutate("/api/rpc/getFoldersAll");
-    }
-    if (ev.folders != null) {
-      await fetch("/api/rpc/moveFolders/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          parentFolderId: ev.target.id,
-          folderIds: ev.folders.map(n => n.id).filter(id => id !== ev.target.id),
-        })
-      });
-
-      mutateNotesParent();
-      mutate("/api/rpc/getFoldersAll");
-    }
-  }
-
 
   const notes = notesParent?.notes ?? [];
   return (
