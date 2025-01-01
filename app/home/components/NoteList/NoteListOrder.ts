f7bf250f63c5e24f56ea08843c1f20bbe760b5b3
k-mitani <k-mitani@unique21.co.jp>
2025-01-02 00:47:05 +0900

    名前 昇順降順間違い修正

--- app/home/components/NoteList/NoteListOrder.ts
+++ app/home/components/NoteList/NoteListOrder.ts
@@ -23,6 +23,6 @@ export const orderItems = [
   oi($Enums.NotesOrder.CREATED_AT_DESC, "作成順↓", (a, b) => b.createdAt.getTime() - a.createdAt.getTime()),
   oi($Enums.NotesOrder.UPDATED_AT_ASC, "更新順↑", (a, b) => (a.updatedAt ?? a.createdAt).getTime() - (b.updatedAt ?? b.createdAt).getTime()),
   oi($Enums.NotesOrder.UPDATED_AT_DESC, "更新順↓", (a, b) => (b.updatedAt ?? b.createdAt).getTime() - (a.updatedAt ?? a.createdAt).getTime()),
-  oi($Enums.NotesOrder.TITLE_ASC, "名前順↑", (a, b) => b.title.localeCompare(a.title)),
-  oi($Enums.NotesOrder.TITLE_DESC, "名前順↓", (a, b) => a.title.localeCompare(b.title)),
+  oi($Enums.NotesOrder.TITLE_ASC, "名前順↑", (a, b) => a.title.localeCompare(b.title)),
+  oi($Enums.NotesOrder.TITLE_DESC, "名前順↓", (a, b) => b.title.localeCompare(a.title)),
 ];
