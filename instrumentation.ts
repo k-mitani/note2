224f670d721bd1e64e664551abc9b8e1c0ffbb78
k-mitani <k-mitani@unique21.co.jp>
2026-05-26 22:01:45 +0900

    chore: 依存関係のアップデート（Next.js, React, TypeScript等）と初期設定の改善

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,8 +1,7 @@
-import { start as registerBackupTask } from './lib/tasks/backup'
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     if (process.env.NODE_ENV === 'production') {
+      const { start: registerBackupTask } = await import('./lib/tasks/backup');
       registerBackupTask();
     }
   }
