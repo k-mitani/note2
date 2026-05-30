c8ed7dc5edc2693d894fb2b2121f915cf7a9c399
k-mitani <k-mitani@unique21.co.jp>
2026-05-30 17:43:08 +0900

    deploy: デプロイスクリプトに prisma generate を追加

--- commands/deploy-note2.sh
+++ commands/deploy-note2.sh
@@ -6,6 +6,7 @@ export PATH="/home/ubuntu/.nvm/versions/node/v24.14.1/bin:$PATH"
 cd /soft/note2
 
 npx prisma migrate deploy
+npx prisma generate
 npx next build
 sudo systemctl restart note2.service
 sudo systemctl status note2.service --no-pager
